65950a0ced78bb4d419b918772d8642f0e09b48f
Mohammad Allam <mallam@cloudinn.net>
2018-07-16 03:20:43 +0200

    Fix bugs, add missing 'droplet_key' and update README

--- assets/common.sh
+++ assets/common.sh
@@ -4,21 +4,32 @@ set -eu
 
 payload=$(cat) # reading input from stdin (source/params/etc..)
 
+# if
+check_ref(){
+  REF=$(echo "$payload" | jq -r '.version.ref // ""')
+}
+
 # initializing variables
 init_vars(){
   GENERATED_WORKER_NAME=$(echo "ci-worker-$BUILD_PIPELINE_NAME-$BUILD_JOB_NAME" | sed -r 's/[^a-zA-Z0-9]+/-/g')
   READ_WORKER_NAME=$(echo "$payload" | jq -r '.params.worker_name // ""')
   WORKER_NAME=${READ_WORKER_NAME:-$GENERATED_WORKER_NAME}
-  DO_API_KEY=$(echo "$payload" | jq -r '.source.api_key // ""')
+  DO_API_KEY=$(echo "$payload" | jq -er '.source.api_key // ""')
   DO_REGION=$(echo "$payload" | jq -r '.source.region // "ams3"')
   DO_SIZE=$(echo "$payload" | jq -r '.source.droplet_size // "s-2vcpu-2gb"')
+  DO_DROPLET_KEY=$(echo "$payload" | jq -er '.source.droplet_key // ""')
   CO_WEB_HOST="$ATC_EXTERNAL_URL"
-  CO_WORKER_KEY=$(echo "$payload" | jq -r '.source.ci_worker_key // ""')
-  CO_TSA_PUB_KEY=$(echo "$payload" | jq -r '.source.ci_tsa_pub_key // ""')
-  CO_TSA_PORT=$(echo "$payload" | jq -r '.source.ci_tsa_port // "2222"')
+  CO_WORKER_KEY=$(echo "$payload" | jq -er '.source.ci_worker_key // ""')
+  CO_TSA_PUB_KEY=$(echo "$payload" | jq -er '.source.ci_tsa_pub_key // ""')
+  CO_TSA_PORT=$(echo "$payload" | jq -er '.source.ci_tsa_port // "2222"')
   CO_TSA_HOST=$(echo "$ATC_EXTERNAL_URL" | pcregrep -o1 "^(?:https?:)?(?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)"):"$CO_TSA_PORT"
-  FLY_USERNAME=$(echo "$payload" | jq -r '.source.fly_username // ""')
-  FLY_USERNAME=$(echo "$payload" | jq -r '.source.fly_password // ""')
+  FLY_USERNAME=$(echo "$payload" | jq -er '.source.fly_username // ""')
+  FLY_PASSWORD=$(echo "$payload" | jq -er '.source.fly_password // ""')
+
+  keys=$(mktemp -d /var/tmp/do-resource.XXXXXX)
+  echo "$CO_WORKER_KEY" > $keys/worker_key
+  echo "$CO_TSA_PUB_KEY" > $keys/tsa_host_key.pub
+  echo "$DO_DROPLET_KEY" > $keys/id_rsa
 }
 
 init_fly(){
@@ -30,11 +41,6 @@ init_fly(){
   fly -t main l -u$FLY_USERNAME -p$FLY_PASSWORD -c $CO_WEB_HOST
 }
 
-keys=$(mktemp $TMPDIR/wp-resource-data.XXXXXX)
-echo "$CO_WORKER_KEY" > $keys/worker_key
-echo "$CO_TSA_PUB_KEY" > $keys/tsa_host_key.pub
-echo "$DO_VM_KEY" > $keys/id_rsa
-
 get_concourse_version(){
   # get concourse version
   CO_VERSION=$(fly -t main sync | cut -d' ' -f2)
@@ -43,9 +49,8 @@ get_concourse_version(){
 init_ssh(){
 # configure ssh-agent to use the digitalocean private key and bypass hostkey verification
 chmod 0600 $keys/id_rsa
-trap 'kill $SSH_AGENT_PID' 0
-ssh-add $keys/id_rsa >/dev/null
 mkdir -p ~/.ssh
+cp $keys/id_rsa ~/.ssh/
 cat > ~/.ssh/config <<EOF
 StrictHostKeyChecking no
 LogLevel quiet
